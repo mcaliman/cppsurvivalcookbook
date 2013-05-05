0e84583f432d46117b2a7846ade84e2d456a29a8
mcaliman <mcaliman@phobos>
2013-05-05 20:00:58 +0200

    update

---
+++ CppSurvivalCookbook/HelloC.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+/**
+ * This is a C++ Hello World tutorial for beginning C++.
+ */
+int main(int argc, char** argv) {
+    std::cout << "Hello C++!" << std::endl;
+    return 0;
+}
+
