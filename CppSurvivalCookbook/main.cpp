b1ff570785492f3f370e5c533544c8c8e7eeba77
mcaliman <mcaliman@phobos>
2013-05-05 19:54:11 +0200

    init

---
+++ CppSurvivalCookbook/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: mcaliman
+ *
+ * Created on 5 maggio 2013, 19.39
+ * github
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
